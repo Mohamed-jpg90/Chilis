1333a638753f5f9ceb73bd9707fcd68a92bfe002
Mohamed Maged <mhmdmajdmhmd98@gmail.com>
2026-01-16 17:04:50 +0200

    payment update3

--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -28,49 +28,46 @@ function Payment() {
       "https://app.fawaterk.com/fawaterkPlugin/fawaterkPlugin.min.js";
     script.async = true;
 
-  script.onload = () => {
-  const pluginConfig = {
-    envType: "test",
-    hashKey: "e039d57e089ea028360defdaffd54eb79cc47830d0a5ce41a2ca26b06c7cbde1",
-    style: { listing: "horizontal" },
-    version: "0",
+    script.onload = () => {
+      window.pluginConfig = {
+        envType: "test",
+        hashKey: "e039d57e089ea028360defdaffd54eb79cc47830d0a5ce41a2ca26b06c7cbde1",
+        style: { listing: "horizontal" },
+        version: "0",
+        requestBody: {
+          cartTotal: total.toFixed(2),
+          currency: "EGP",
 
-    requestBody: {
-      cartTotal: total.toFixed(2),
-      currency: "EGP",
+          customer: {
+            first_name: username || "Guest",
+            last_name: "User",
+            email: email || "test@test.com",
+            phone: phone || "01000000000",
+            address: "Cairo, Egypt",
+          },
 
-      customer: {
-        first_name: username || "Guest",
-        last_name: "User",
-        email: email || "test@test.com",
-        phone: phone || "01000000000",
-        address: "Cairo, Egypt",
-      },
+          redirectionUrls: {
+            successUrl: "https://chilis-iota.vercel.app/payment/success",
+            failUrl: "https://chilis-iota.vercel.app/payment/failed",
+            pendingUrl: "https://chilis-iota.vercel.app/payment/pending",
+          },
 
-      redirectionUrls: {
-        successUrl: "https://chilis-iota.vercel.app/payment/success",
-        failUrl: "https://chilis-iota.vercel.app/payment/failed",
-        pendingUrl: "https://chilis-iota.vercel.app/payment/pending",
-      },
+          "cartItems": [{
+            "name": "this is test oop 112252",
+            "price": total.toFixed(2),
+            "quantity": "1"
+          },
 
-      cartItems: [
-        {
-          name: "this is test oop 112252",
-          price: total.toFixed(2),
-          quantity: "1",
+          ],
+          "payLoad": {
+            "custom_field1": "xyz",
+            "custom_field2": "xyz2"
+          }
         },
-      ],
+      };
 
-      payLoad: {
-        custom_field1: "xyz",
-        custom_field2: "xyz2",
-      },
-    },
-  };
-
-  window.fawaterkCheckout(pluginConfig);
-};
-
+      window.fawaterkCheckout(window.pluginConfig);
+    };
 
     document.body.appendChild(script);
   }, []);
@@ -80,7 +77,7 @@ function Payment() {
   return (
     <div>
       <h2>Payment</h2>
-      <h3>Total:: {total.toFixed(2)} EGP</h3>
+      <h3>Total: {total.toFixed(2)} EGP</h3>
 
       {/* REQUIRED */}
       <div id="fawaterkDivId"></div>
